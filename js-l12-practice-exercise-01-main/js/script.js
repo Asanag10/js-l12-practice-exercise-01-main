c88e6e6c38e1f1a6fca58fd445e57b90f5332079
grigsby_a <asanag10@gmail.com>
2024-10-08 10:23:31 -0500

    first commit

---
+++ js-l12-practice-exercise-01-main/js/script.js
@@ -0,0 +1,8 @@
+const getData = async function() {
+    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await usersRequest.json();
+    console.log(data);    
+};
+getData();
+
+
