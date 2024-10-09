2e049182a5f926109dca98ce4cd39b31cdf381ca
Asana Grigsby <62454405+Asanag10@users.noreply.github.com>
2024-10-09 11:53:31 -0500

    Update script.js

--- js-l12-practice-exercise-01-main/js/script.js
+++ js-l12-practice-exercise-01-main/js/script.js
@@ -5,33 +5,37 @@ const getData = async function () {
   const data = await usersRequest.json();
   //console.log(data);
 
+   //map the variable "userResults" to the property name, "results", for the array of objects.
   const userResults = data.results;
-  //map the variable "userResults" to the property name, "results", for the array of objects.
-
+  
   console.log(userResults);
 
-  displayUsers(userResults); // Call displayUsers and pass userResults
+  // Call displayUsers and pass userResults
+  displayUsers(userResults); 
 };
 
 getData();
 
+//#7 of exercise 1 - create a new function expression...
 const displayUsers = function (userResults) {
-  //#7 of exercise 1
   randomFolks.innerHTML = ""; //emptying/clear the randomFolks element’s contents
 
+  //#8 of exercise 1 - loop over userResults...
   for (const user of userResults) {
     const country = user.location.country;
     const name = user.name.first;
     const imageUrl = user.picture.medium;
     const userDiv = document.createElement("div");
-    //#8 of exercise 1
+    
+    //#9 of exercise 1 - create a div element...
     userDiv.innerHTML = ` 
         <h3>${name}</h3>
         <p>${country}</p>
         <img src=${imageUrl} alt="User avatar" />
-    `; //#9 of exercise 1
+    `; 
 
-    randomFolks.append(userDiv); //#10. Append userDiv to the randomFolks element.
+    //#10 of exercise 1 Append userDiv to the randomFolks element.
+    randomFolks.append(userDiv); 
   }
 };
 
